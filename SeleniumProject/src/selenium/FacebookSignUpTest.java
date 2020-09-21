d524fa58ac65f87f366cf100daba02625bc682c9
LAPTOP-VIVEK\chatr <chatrolavivek@gmail.com>
2020-09-20 22:19:13 -0400

    ## updated code using explicit wait time

--- SeleniumProject/src/selenium/FacebookSignUpTest.java
+++ SeleniumProject/src/selenium/FacebookSignUpTest.java
@@ -3,6 +3,9 @@ package selenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import common.resources.CommonResources;
 
@@ -13,8 +16,8 @@ public class FacebookSignUpTest {
 
 		System.setProperty("webdriver.gecko.driver", CommonResources.geckoDriverLocation);
 		FirefoxDriver driver = new FirefoxDriver();
-
 		driver.get("https://www.facebook.com/");
+
 		driver.findElement(By.xpath("//a[@data-testid='open-registration-form-button']")).click();
 
 		Thread.sleep(2000);
@@ -31,14 +34,37 @@ public class FacebookSignUpTest {
 		WebElement password = driver.findElement(By.id("password_step_input"));
 		password.sendKeys("passwosjas");
 
+		WebElement month = driver.findElement(By.id("month"));
+		Select m = new Select(month);
+		m.selectByValue("7");
+
+		WebElement day = driver.findElement(By.id("day"));
+		Select d = new Select(day);
+		d.selectByIndex(12);
+
+		WebElement year = driver.findElement(By.id("year"));
+		Select y = new Select(year);
+		y.selectByVisibleText("1996");
+
+		WebElement maleButton = driver.findElement(By.xpath("//input[@value='2']"));
+		// (//input[@class='_8esa'])[2]
+		maleButton.click();
+
+		WebElement signUp = driver.findElement(By.name("websubmit"));
+		signUp.click();
+
 		driver.findElement(By.name("websubmit")).click();
+
+		// Thread.sleep(2000);
+		WebDriverWait wait = new WebDriverWait(driver, 30);
 
 		String expectedFirstNameError = "What’s your name?";
 
+		wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//div[text()='What’s your name?']"),
+				"What’s your name?"));
 		String actualFirstNameError = driver.findElement(By.xpath("//div[text()='What’s your name?']")).getText();
-		// String actualFirstNameError =
-		// driver.findElement(By.xpath("//div[@class='_5v-0 _53im']/div")).getText();
-		// //div[contains(@id,'js_')]
+
+		// //div[@class='_5v-0 _53im']/div //div[contains(@id,'js_')]
 
 		System.out.println("First Name missing Error: " + actualFirstNameError);
 
