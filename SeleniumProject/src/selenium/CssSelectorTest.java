eaf1883087e68cc14c831c460adad8cc1d40e3ae
LAPTOP-VIVEK\chatr <chatrolavivek@gmail.com>
2020-09-20 22:20:59 -0400

    ## added code to find elements using CSS Selector

---
+++ SeleniumProject/src/selenium/CssSelectorTest.java
@@ -0,0 +1,22 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import common.resources.CommonResources;
+
+public class CssSelectorTest {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.gecko.driver", CommonResources.geckoDriverLocation);
+
+		FirefoxDriver driver = new FirefoxDriver(); // to run in Firefox browser
+		driver.get("https://www.facebook.com/");
+
+		driver.findElement(By.cssSelector("#email")).sendKeys("hvhd");
+		driver.findElement(By.cssSelector("input[id^='pas']")).sendKeys("hd");
+		driver.findElement(By.cssSelector("button[name='login']")).click();
+	}
+
+}
